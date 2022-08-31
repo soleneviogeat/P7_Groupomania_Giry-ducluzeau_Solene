51c13c5b06987ea5a49c30c698305eb6daede9db
Solène Viogeat <so.gd@hotmail.fr>
2022-08-31 08:05:27 +0200

    mise en place du header de la page d'accueil

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledLink } from '../utils/Atoms'
+import LightLogo from '../assets/icon-left-font-monochrome-white.png'
+import DarkLogo from '../assets/icon-left-font.png'
+import { useTheme } from '../utils/hooks'
+
+const HomeLogo = styled.img`
+  height: 0%;
+  width: 17rem;
+`
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+function Header() {
+  const { theme } = useTheme()
+
+  return (
+    <NavContainer>
+      <Link to="/">
+        <HomeLogo src={theme === 'light' ? DarkLogo : LightLogo} />
+      </Link>
+      <div>
+        <StyledLink $theme={theme} to="/">
+          Accueil
+        </StyledLink>
+        
+
+
+
+      </div>
+    </NavContainer>
+  )
+}
+
+export default Header
+
+
+/*<StyledLink $theme={theme} to="/freelances">
+          Profils
+        </StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>*/
